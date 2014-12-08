25a100c75db1b8985fadc73f514a3c6323b25e0d
schyzoo <schyzoo@gmail.com>
2014-12-08 17:52:00 +0300

    initial commit

---
+++ gallery.js
@@ -0,0 +1,19 @@
+var Gallery = function(){
+    function Gallery(elem) {
+        this._$elem = $(elem);
+        this._$items = this._$elem.find('.slider__item')
+            .detach();
+        this._$viewport = $('<div class="slider__viewport"/>');
+        this._$canvas = $('<div class="slider__canvas"/>')
+            .appendTo(this._$viewport);
+        this._$items.appendTo(this._$canvas);
+        this._$viewport.appendTo('body');//.replace(this._$elem);
+        
+        this._$canvas.css({
+            'overflow': 'hidden',
+            width: this._$items.width() * this._$items.length
+        });
+    }
+    
+    return Gallery;
+}();
